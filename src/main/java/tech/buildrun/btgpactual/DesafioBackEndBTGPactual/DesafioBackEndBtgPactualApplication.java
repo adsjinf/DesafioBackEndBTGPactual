19e22ad7889f96d5c0ee208edd3207973f56e303
adsjinf <jinfrj@hotmail.com>
2024-08-15 22:28:34 -0300

    Inicializando o Spring Boot

---
+++ src/main/java/tech/buildrun/btgpactual/DesafioBackEndBTGPactual/DesafioBackEndBtgPactualApplication.java
@@ -0,0 +1,13 @@
+package tech.buildrun.btgpactual.DesafioBackEndBTGPactual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBackEndBtgPactualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBackEndBtgPactualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/buildrun/btgpactual/DesafioBackEndBTGPactual/DesafioBackEndBtgPactualApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.buildrun.btgpactual.DesafioBackEndBTGPactual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBackEndBtgPactualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
